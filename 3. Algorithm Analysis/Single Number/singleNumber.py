0a59f1258e700a05d310e2e732ede72dbf06294b
SeonWoo05 <tjsdn000508@gmail.com>
2024-10-04 22:30:47 +0900

    code

--- 3. Algorithm Analysis/Single Number/singleNumber.py
+++ 3. Algorithm Analysis/Single Number/singleNumber.py
@@ -1,1 +1,9 @@
-sdsd
+class Solution(object):
+    def singleNumber(self, nums):
+        A = set()
+        for i in nums:
+            if i in A:
+                A.remove(i)
+            else:
+                A.add(i)
+        return A.pop()
