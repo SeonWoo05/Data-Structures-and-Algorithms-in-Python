2091d152ae58e116251b8fbfe1156dee448eea12
SeonWoo05 <tjsdn000508@gmail.com>
2024-11-01 22:16:29 +0900

    .

--- 3. Algorithm Analysis/Power of Two.py
+++ 3. Algorithm Analysis/Power of Two.py
@@ -15,7 +15,7 @@ def isPowerOfTwo(n):
 def isPowerOfTwo(n):
     if n == 0:
         return False
-    diff_bits = n & (n-1)
+    diff_bits = n & (n-1) # 비트연산 : 2의 n제곱수와 2의 (n-1)제곱수는 AND 연산하면 0임
     if diff_bits == 0:
         return True
     else:
