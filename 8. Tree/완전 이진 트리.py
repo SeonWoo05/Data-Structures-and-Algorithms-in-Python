098f657b77507167ea1c744afd95b3f84d93454f
SeonWoo05 <tjsdn000508@gmail.com>
2024-11-13 17:12:31 +0900

    완전 이진 트리

---
+++ 8. Tree/완전 이진 트리.py
@@ -0,0 +1,24 @@
+# BJ 9934
+
+# 중위순회로 읽은 노드들을 주면 레벨 나눠서 단계별로 출력하기
+def tree_by_level(K,values):
+    def build_tree(level,start,end):
+        if level >= K or start > end:
+            return
+        
+        mid = (start + end) // 2
+        levels[level].append(values[mid])
+
+        build_tree(level+1,start,mid-1)
+        build_tree(level+1,mid+1,end)
+
+    levels = [[] for _ in range(K)]
+    build_tree(0,0,len(values)-1)
+    return levels
+
+K = int(input())
+values = list(map(int, input().split()))
+
+result = tree_by_level(K,values)
+for level in result:
+    print(*level)
